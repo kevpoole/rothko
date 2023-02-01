b772332d4ee8cb4895266d95b0efb7afe1fa97b0
Kevin Poole <kevitron@hotmail.com>
2023-01-31 20:56:25 -0500

    webp

--- src/App.jsx
+++ src/App.jsx
@@ -10,55 +10,37 @@ const arrs = [
   ["one", "two", "three", "blank"],
 ];
 
-// const colors = [
-//   // ,
-//   "#da9e00",
-//   "#c13c1f",
-//   "#ddc18a",
-//   "#c5aa5c",
-//   "#7a0510",
-//   "#032e36",
-//   "#577e85",
-//   "#23166d",
-//   "#2c5568",
-// ]
-
 const masks = [
-  "/images/rothkomask1.png",
-  "/images/rothkomask3.png",
-  "/images/rothkomask4.png",
-  "/images/rothkomask2.png",
+  "/images/rothkomask1.webp",
+  "/images/rothkomask3.webp",
+  "/images/rothkomask4.webp",
+  "/images/rothkomask2.webp",
 ];
 
 const stripes = [
-  "/images/rothkostripe.png",
-  "/images/rothkostripe2.png",
-  "/images/rothkostripe3.png",
+  "/images/rothkostripe.webp",
+  "/images/rothkostripe2.webp",
+  "/images/rothkostripe3.webp",
 ];
 
-const stripeClassName = [
-  "stripe", "blank"
-]
+const stripeClassName = ["stripe", "blank"];
 
-const blendMode = [ "multiply"];
 
 const flex = ["column", "column-reverse"];
 
 function App() {
   const [color, setColor] = useState();
   const [fields, setFields] = useState(arrs[1]);
-  const [stripe, setStripe] = useState();
-  const [height, setHeight] = useState("auto");
-  const [height3, setHeight3] = useState("auto");
+  const [height, setHeight] = useState();
+  const [height3, setHeight3] = useState();
   const [width, setWidth] = useState();
   const [width2, setWidth2] = useState();
   const [column, setColumn] = useState(flex[0]);
   const [gap, setGap] = useState();
-  const [blend, setBlend] = useState("");
-  const [blend2, setBlend2] = useState("");
   const [mask, setMask] = useState(masks[0]);
   const [mask2, setMask2] = useState(masks[0]);
-  const [stripeClass, setStripeClass] = useState()
+  const [stripe, setStripe] = useState();
+  const [stripeClass, setStripeClass] = useState();
 
   // const randColor = () => Math.random().toString(16).substring(2, 8);
 
@@ -76,14 +58,12 @@ function App() {
     setHeight3(getRndInteger(50, 80));
     setColumn(flex[getRndInteger(0, 1)]);
     setGap(getRndInteger(-1, 0));
-    setBlend(blendMode[getRndInteger(0, blendMode.length - 1)]);
-    setBlend2(blendMode[getRndInteger(0, blendMode.length - 1)]);
     setMask(masks[getRndInteger(0, masks.length - 1)]);
     setMask2(masks[getRndInteger(0, masks.length - 1)]);
     setWidth(getRndInteger(85, 98));
     setWidth2(getRndInteger(80, 100));
     setStripe(stripes[getRndInteger(0, stripes.length - 1)]);
-    setStripeClass(stripeClassName[getRndInteger(0, 1)])
+    setStripeClass(stripeClassName[getRndInteger(0, 1)]);
   }
 
   return (
@@ -95,12 +75,7 @@ function App() {
           backgroundColor: color,
         }}
       >
-        <div
-          className={fields[0]}
-          style={{
-            mixBlendMode: `${blend2}`,
-          }}
-        >
+        <div className={fields[0]}>
           <img src={mask2} alt="" />
         </div>
         <div className={stripeClass}>
@@ -119,9 +94,8 @@ function App() {
         <div
           className={fields[2]}
           style={{
-            margin: `${gap}rem`,
+            // margin: `${gap}rem`,
             height: `${height3}%`,
-            mixBlendMode: `${blend}`,
             width: `${width2}%`,
           }}
         >
