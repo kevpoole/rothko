c675032da41e9fbc91811106e5bc9ee73eafada5
Kevin Poole <kevitron@hotmail.com>
2023-02-02 02:49:28 -0500

    state objects

--- src/App.jsx
+++ src/App.jsx
@@ -25,43 +25,35 @@ const stripes = [
 
 const stripeClassName = ["stripe", "blank"];
 
-
 const flex = ["column", "column-reverse"];
 
 function App() {
   const [color, setColor] = useState();
   const [fields, setFields] = useState(arrs[1]);
-  const [height, setHeight] = useState();
-  const [height3, setHeight3] = useState();
-  const [width, setWidth] = useState();
-  const [width2, setWidth2] = useState();
+  const [height, setHeight] = useState({one: 100, two: 100 });
+  const [width, setWidth] = useState({one: 100, two: 100,});
   const [column, setColumn] = useState(flex[0]);
-  const [gap, setGap] = useState();
-  const [mask, setMask] = useState(masks[0]);
-  const [mask2, setMask2] = useState(masks[0]);
+  const [mask, setMask] = useState({ one: masks[0], two: masks[1] });
   const [stripe, setStripe] = useState();
   const [stripeClass, setStripeClass] = useState();
 
   // const randColor = () => Math.random().toString(16).substring(2, 8);
 
-  // const randOpacity = () => (Math.random() * 5000).toString(16).substring(6, 8);
-
   function handleRothko() {
     setFields(arrs[getRndInteger(0, arrs.length - 1)]);
+    setMask({
+      one: masks[getRndInteger(0, masks.length - 1)],
+      two: masks[getRndInteger(0, masks.length - 2)],
+    });
     setColor(
-      `rgb(${getRndInteger(0, 255)}, ${getRndInteger(0, 100)}, ${getRndInteger(
-        0,
-        255
-      )})`
+      `rgb(${getRndInteger(0, 255)}, 
+      ${getRndInteger(0, 100)}, 
+      ${getRndInteger(0, 255)})`
     );
-    setHeight(getRndInteger(5, 70));
-    setHeight3(getRndInteger(50, 80));
+    setHeight({one: getRndInteger(5, 70), two: getRndInteger(50, 80)});
+    setWidth({one: getRndInteger(75, 90),two: getRndInteger(80, 100),
+    });
     setColumn(flex[getRndInteger(0, 1)]);
-    setGap(getRndInteger(-1, 0));
-    setMask(masks[getRndInteger(0, masks.length - 1)]);
-    setMask2(masks[getRndInteger(0, masks.length - 1)]);
-    setWidth(getRndInteger(85, 98));
-    setWidth2(getRndInteger(80, 100));
     setStripe(stripes[getRndInteger(0, stripes.length - 1)]);
     setStripeClass(stripeClassName[getRndInteger(0, 1)]);
   }
@@ -76,7 +68,7 @@ function App() {
         }}
       >
         <div className={fields[0]}>
-          <img src={mask2} alt="" />
+          <img src={mask.two} alt="" />
         </div>
         <div className={stripeClass}>
           <img src={stripe} alt="" />
@@ -84,22 +76,21 @@ function App() {
         <div
           className={fields[1]}
           style={{
-            height: `${height}%`,
-            width: `${width}%`,
+            height: `${height.one}%`,
+            width: `${width.one}%`,
           }}
         >
-          <img src={mask} alt="" />
+          <img src={mask.one} alt="" />
         </div>
 
         <div
           className={fields[2]}
           style={{
-            // margin: `${gap}rem`,
-            height: `${height3}%`,
-            width: `${width2}%`,
+            height: `${height.two}%`,
+            width: `${width.two}%`,
           }}
         >
-          <img src={mask} alt="" />
+          <img src={mask.one} alt="" />
         </div>
       </div>
       <button onClick={handleRothko}>rothko me</button>
